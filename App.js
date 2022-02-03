03971d1d382603f8f79e07df18c20e2f8316b4fd
dariush <dariushtaghavi9797@gmail.com>
2022-02-03 17:18:45 +0330

    add navigation

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {} from 'react-native';
+import {Text} from 'react-native';
 import {RestaurantsScreen} from './src/features/restaurants/screens/RestaurantsScreen';
 import {ThemeProvider} from 'styled-components/native';
 import {theme} from './src/infrastructre/theme';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {SafeArea} from './src/components/utility/SafeArea';
+const Tab = createMaterialBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RestaurantsScreen />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Screen name="Map" component={Map} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
