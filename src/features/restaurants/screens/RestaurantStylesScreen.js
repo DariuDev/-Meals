991b2216ffa99f967b82752b4ff79dbf4c4a2dd7
dariush <dariushtaghavi9797@gmail.com>
2022-02-03 12:34:07 +0330

    divide styles

---
+++ src/features/restaurants/screens/RestaurantStylesScreen.js
@@ -0,0 +1,13 @@
+import {} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
+import styled from 'styled-components/native';
+
+export const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+`;
+export const SearchContainer = styled(View)`
+  margin: 2%;
+`;
+export const RestaurantListContainer = styled(View)`
+  flex: 1;
+`;
