efabed5f75890540cd9feb131122dad79473c4a0
dariush <dariushtaghavi9797@gmail.com>
2022-02-04 14:11:17 +0330

    add tabs icon

--- src/features/restaurants/screens/RestaurantStylesScreen.js
+++ src/features/restaurants/screens/RestaurantStylesScreen.js
@@ -1,10 +1,7 @@
 import {} from 'react-native';
-import {SafeAreaView, View} from 'react-native';
+import {View} from 'react-native';
 import styled from 'styled-components/native';
 
-export const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-`;
 export const SearchContainer = styled(View)`
   margin: 2%;
 `;
