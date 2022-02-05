3d0e74bfc88d2eb713795f608ae8cee3fd3d584a
dariush <dariushtaghavi9797@gmail.com>
2022-02-05 17:45:47 +0330

    add activity indicator

--- src/features/restaurants/screens/RestaurantStylesScreen.js
+++ src/features/restaurants/screens/RestaurantStylesScreen.js
@@ -1,7 +1,12 @@
-import {} from 'react-native';
 import {View} from 'react-native';
 import styled from 'styled-components/native';
 
 export const SearchContainer = styled(View)`
   margin: 2%;
+`;
+export const ActivityIndicatorContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  right: 50%;
 `;
--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -1,16 +1,23 @@
 import React, {useContext} from 'react';
 import {FlatList} from 'react-native';
-import {Searchbar} from 'react-native-paper';
+import {Searchbar, ActivityIndicator} from 'react-native-paper';
 import {RestaurantsComponent} from '../components/RestaurantsComponent';
-import {SearchContainer} from './RestaurantStylesScreen';
+import {
+  SearchContainer,
+  ActivityIndicatorContainer,
+} from './RestaurantStylesScreen';
 import {SafeArea} from '../../../components/utility/SafeArea';
 import {RestaurantsContext} from '../../../services/restaurants/RestaurantsContext';
 
 export const RestaurantsScreen = () => {
   const {restaurants, isLoading, error} = useContext(RestaurantsContext);
-  console.log(error);
   return (
     <SafeArea>
+      {isLoading && (
+        <ActivityIndicatorContainer>
+          <ActivityIndicator size={50} animating={true} color={'cyan'} />
+        </ActivityIndicatorContainer>
+      )}
       <SearchContainer>
         <Searchbar placeholder={'search'} />
       </SearchContainer>
