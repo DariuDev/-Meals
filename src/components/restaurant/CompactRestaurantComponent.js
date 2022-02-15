78b09b282326af82762fca9d061ab675ab6aebb5
dariush <dariushtaghavi9797@gmail.com>
2022-02-15 22:48:36 +0330

    custom map callout

---
+++ src/components/restaurant/CompactRestaurantComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, Platform} from 'react-native';
+import {CompactImage, Item, CompactWebview} from './CompactStyle';
+
+const isAndroid = Platform.OS === 'android';
+
+export const CompactRestaurantComponent = ({restaurant}) => {
+  const Image = isAndroid ? CompactWebview : CompactImage;
+  return (
+    <Item>
+      <Image source={{uri: restaurant.photos[0]}} />
+      <Text>{restaurant.name}</Text>
+    </Item>
+  );
+};
