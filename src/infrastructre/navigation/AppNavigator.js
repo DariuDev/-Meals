53f72b5a9373ed6e0b5885ca223ba9c5445f66b9
dariush <dariushtaghavi9797@gmail.com>
2022-03-01 12:05:51 +0330

    add settings navigation

--- src/infrastructre/navigation/AppNavigator.js
+++ src/infrastructre/navigation/AppNavigator.js
@@ -1,6 +1,4 @@
-import React, {useContext} from 'react';
-import {Text, Button} from 'react-native';
-import {SafeArea} from '../../components/utility/SafeArea';
+import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {RestaurantsContextProvider} from '../../services/restaurants/RestaurantsContext';
@@ -10,20 +8,10 @@ import {FavouritesContextProvider} from '../../services/favourites/FavouritesCon
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {RestaurantNavigator} from './RestaurantsNavigator';
 import {MapScreen} from '../../features/map/screens.js/MapScreen';
-
-import {AuthenticationContext} from '../../services/authentication/AuthenticationContext';
+import {SettingsNavigator} from '../../infrastructre/navigation/SettingsNavigator';
 
 const Tab = createMaterialBottomTabNavigator();
 
-const Settings = () => {
-  const {onLogout} = useContext(AuthenticationContext);
-  return (
-    <SafeArea>
-      <Text>Settings</Text>
-      <Button title="logout" onPress={() => onLogout()} />
-    </SafeArea>
-  );
-};
 // const Map = () => (
 //   <SafeArea>
 //     <Text>Map</Text>
@@ -53,7 +41,7 @@ export const AppNavigator = () => (
           })}>
           <Tab.Screen name="Restaurants" component={RestaurantNavigator} />
           <Tab.Screen name="Map" component={MapScreen} />
-          <Tab.Screen name="Settings" component={Settings} />
+          <Tab.Screen name="Settings" component={SettingsNavigator} />
         </Tab.Navigator>
       </RestaurantsContextProvider>
     </LocationContextProvider>
